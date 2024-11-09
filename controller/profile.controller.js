3df9d840783234f558d60ad1057d937f4eafa708
Nihad <mncnihad@gmail.com>
2024-11-09 07:47:17 +0530

    user data displayed profile page

--- controller/profile.controller.js
+++ controller/profile.controller.js
@@ -1,9 +1,28 @@
-const getProfile = (req, res, next) => {
-  res.render("users/Profile", {
-    title: "Profile Page",
-    isProfilePage: true,
-    user: req.session.user,
-  });
-};
+const User = require("../model/User");
 
+const getProfile = async (req, res, next) => {
+  const user = req.session.user;
+  if (!user) {
+    return res.status(401).json({ message: "User is not logged in." });
+  }
+
+  try {
+    const userData = await User.findOne({ email: user.email });
+    if (!userData) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    res.render("users/Profile", {
+      title: "Profile Page",
+      isProfilePage: true,
+      email: userData.email,
+      user: req.session.user,
+      username: userData.username,
+      password: userData.password,
+    });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
 module.exports = { getProfile };
