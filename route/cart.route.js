90e37d717c2004899c2644d6700a7cb4d83b5bb7
Nihad <mncnihad@gmail.com>
2024-11-13 22:41:00 +0530

    addto cart route adn controolere\

--- route/cart.route.js
+++ route/cart.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { addToCart } = require("../controller/cart.controller");
 const router = express.Router();
 
-router.post("/addtocart", addToCart);
+router.post("/addtocart/:id", addToCart);
 
 module.exports = router;
