2d65491a92608bb8fdaa0ee03dcedc495d802188
Nihad <mncnihad@gmail.com>
2024-11-22 18:52:05 +0530

    changed  bcrypt to bcyptjs

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require("../model/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto"); // For OTP generation
