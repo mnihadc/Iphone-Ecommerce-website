f0c688b2e5ef2c965fcdc40ab258d2c7f5cccf29
Nihad <mncnihad@gmail.com>
2024-11-24 07:03:10 +0530

    updated

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(
     credentials: true, // Allow credentials (cookies)
   })
 );
+
 app.use(passport.initialize());
 passport.use(
   new GoogleStrategy(
@@ -53,7 +54,6 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:3000/auth/google/callback",
-      scope: ["profile", "email"], // Ensure to request profile and email
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -76,27 +76,6 @@ passport.use(
       }
     }
   )
-);
-
-// Routes for Google authentication
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/", session: false }), // <-- session: false added
-  (req, res) => {
-    // Successful login
-    res.cookie("authToken", req.user.token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Make sure to set to true in production
-      sameSite: "strict",
-      maxAge: 3600000, // 1 hour
-    });
-    res.redirect("/profile/user"); // Redirect to the profile page or any other protected route
-  }
 );
 
 const hbs = exphbs.create({
